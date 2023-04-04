fc10893a935c4f4fc7731c366da88a61067721d7
Kyungwon <75094086+jing54321@users.noreply.github.com>
2023-04-03 20:37:02 -0400

    Update GameResult.js

--- frontend/Screen/GameResult.js
+++ frontend/Screen/GameResult.js
@@ -1,6 +1,4 @@
-/**
- StAuth10244: I Kyungwon Lee, 000865096 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
-*/
+
 import { StyleSheet, Text, View} from 'react-native';
 import { Button } from "@react-native-material/core";
 
