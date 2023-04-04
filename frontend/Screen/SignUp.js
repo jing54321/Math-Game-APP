b7ee13c881c6f331ceba940aed3fcdf3ac005574
Kyungwon <75094086+jing54321@users.noreply.github.com>
2023-04-03 20:37:54 -0400

    Update SignUp.js

--- frontend/Screen/SignUp.js
+++ frontend/Screen/SignUp.js
@@ -1,6 +1,4 @@
-/**
- StAuth10244: I Kyungwon Lee, 000865096 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
-*/
+
 import {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 import {Stack, ActivityIndicator } from "@react-native-material/core";
